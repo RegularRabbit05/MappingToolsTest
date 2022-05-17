c30a5b7e75f2cb87bf20b46d6516b910044e876f
Reg <69511985+RegularRabbit05@users.noreply.github.com>
2022-05-17 14:16:58 +0200

    Update terrain.py

--- terrain.py
+++ terrain.py
@@ -3,18 +3,18 @@ from PIL import Image
 
 im = Image.open("terrain.png")   
 
-trees = []
+tiles = []
 
 width, height = im.size
 for x in range(width):
     for y in range(height):
         r,g,b = im.getpixel((x,y))
         if r > 128:
-            trees.append([float(x),float(0.0),float(y),float(g)])
+            tiles.append([float(x),float(0.0),float(y),float(g)])
 
 bytesMap = []
 
-for t in trees:
+for t in tiles:
     l1, l2, l3, t = list(struct.pack("f", t[0])), list(struct.pack("f", t[1])), list(struct.pack("f", t[2])), list(struct.pack("f", t[3]))
     bytesMap.append(l1[0])
     bytesMap.append(l1[1])
